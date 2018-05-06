2202ad850cf4eb0adf4a2fc80ce3cddbad391bc9
Gregory Manley <gregory.f.manley@gmail.com>
2018-05-06 19:10:01 -0400

    Update config.js

--- website/config.js
+++ website/config.js
@@ -1,61 +1,57 @@
-var api = "http://poolhost.com:1117";
+var poolHost = "pool.turtlefaucet.ga";
 
-var api_blockexplorer = "http://daemonhost.com:1118";
+var api = "http://" + poolHost + ":8117";
 
-var poolHost = "poolhost.com";
+var api_blockexplorer = "http://"+ poolHost +":11899";
 
-var irc = "irc.freenode.net/#poolhost";
+var irc = "";
 
-var email = "support@poolhost.com";
+var email = "";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "https://blocks.turtle.link/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "https://blocks.turtle.link/?hash={id}#blockchain_transaction";
+
+function reloadPoolHostURLs(){
+    api = "http://" + poolHost + ":8117";
+    api_blockexplorer = "http://"+ poolHost +":11899";
+    //blockchainExplorer = "http://"+ poolHost +"/?hash={id}#blockchain_block";
+    //transactionExplorer = "http://"+ poolHost +"/?hash={id}#blockchain_transaction";
+}
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = {
-    "bcn": [
-        ["bcn.mypool.online", "http://bcn.mypool.online:8084"],
-        ["democats.org", "http://pool.democats.org:7603"]
-    ],
-    "bip": [
-        ["bip.mypool.online", "http://bip.mypool.online:18874"],
-        ["democats.org", "http://pool.democats.org:7693"],
-        ["bip.cryptonotepool.com", "http://5.189.135.137:8121"],
-        ["bip.ms-pool.net.ua", "http://bip.ms-pool.net.ua:8117"],
-        ["bip.crypto-coins.club", "http://bip.crypto-coins.club:8118"]
-    ],
-    "coal": [
-        ["coal.mypool.online", "http://coal.mypool.online:7704"],
-        ["democats.org", "http://pool.democats.org:7703"]
-    ],
-    "dsh": [
-        ["dsh.mypool.online", "http://dsh.mypool.online:29084"],
-        ["democats.org", "http://pool.democats.org:7613"]
-    ],
-    "fcn": [
-        ["fcn.mypool.online", "http://fcn.mypool.online:24084"]
-    ],
-    "krb": [
-        ["krb.mypool.online", "http://krb.mypool.online:32351"],
-        ["democats.org", "http://pool2.democats.org:7673"],
-        ["pool.karbowanec.com", "http://pool.karbowanec.com:8117"],
-        ["pool2.karbowanec.com", "http://pool2.karbowanec.com:8117"],
-        ["krb.sberex.com", "http://krb.sberex.com:7006"],
-        ["krb.crypto-coins.club", "http://krb.crypto-coins.club:8118"],
-        ["krb.cryptonotepool.com", "http://5.189.135.137:8618"],
-        ["krbpool.ml", "http://krbpool.ml:8117"]
-    ],
-    "qcn": [
-        ["qcn.mypool.online", "http://qcn.mypool.online:23084"]
-    ],
-    "xci": [
-        ["xci.mypool.online", "http://xci.mypool.online:42004"],
-        ["xci.cryptonotepool.com", "http://5.189.135.137:8119"]
+    "trtl": [
+        /*["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
+        ["us.turtlepool.space", "http://us.turtlepool.space:8117"],
+        ["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
+        ["slowandsteady.fun", "http://slowandsteady.fun:8119"],
+        ["trtl.mine2gether.com", "https://trtl.mine2gether.com/api/"],
+        ["pool.turtleco.in", "https://pool.turtleco.in/api/"],
+        ["xk.is", "https://xk.is/api/"],
+        ["auspool.turtleco.in", "https://auspool.turtleco.in/api/"],
+        ["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"]
+    ]*/
+        ["eu.turtlepool.space", "http://eu.turtlepool.space/api"],
+        ["us.turtlepool.space", "http://us.turtlepool.space/api"],
+        ["hk.turtlepool.space", "http://hk.turtlepool.space/api"],
+        //["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
+        ["slowandsteady.fun", "http://slowandsteady.fun:8119"],
+        ["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
+        ["pool.turtleco.in", "https://pool.turtleco.in/api"],
+        ["auspool.turtleco.in", "https://auspool.turtleco.in/api"],
+        //["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
+        ["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
+        ["z-pool.com", "https://z-pool.com/api"],
+        ["xk.is", "https://xk.is/api"],
+        //["cryptoknight.cc/turtle", "http://78.46.85.142:6229"],
+        //["pool.trtl.me", "http://pool.trtl.me:8117"],
+        ["trtlpool.ninja", "http://trtlpool.ninja:8117"],
+        ["trtl.ninja", "https://trtl.ninja/api"]
     ]
 };
